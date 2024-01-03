765ab0fb13b8719588392d84c5e014373d9df3c6
Amit.manoshevitz <Amit.manoshevitz@post.runi.ac.il>
2024-01-03 17:12:58 +0200

    first

--- LowerCase.java
+++ LowerCase.java
@@ -1,17 +1,34 @@
-/** String processing exercise 1. */
-public class LowerCase {
-    public static void main(String[] args) {  
-        String str = args[0];
-        System.out.println(lowerCase(str));
-    }
+class LowerCase { 
+    public static void main(String[] args) {
+     	String word = args[0];
+		System.out.println(lowerCase(word));
+	//System.out.println((int) c);
 
-   /**
-    * Returns a string which is identical to the original string, 
-    * except that all the upper-case letters are converted to lower-case letters.
-    * Non-letter characters are left as is.
-    */
-    public static String lowerCase(String s) {
-        // Replace the following statement with your code
-        return null;
+   }
+
+  public static String lowerCase(String word) {
+  	String newword = "";
+        int len = word.length();
+        
+        /////// now Im changing from char to ASCII numbers
+        ////// when there is a capital letter it is in the range of 65 to 90
+        ////// I change it to lowercase letters by bringing them 32 tabs in the ASCII tabel
+        ////// the I change it back to char when the number represent a lower class letter
+
+        for (int i=0;i<len ;i++) {
+      	char c = word.charAt(i);
+      	int d = (int)c;
+      	if (d >= 65 && d <= 90) {
+      		d = d + 32;
+      		char e = (char)d;
+      		newword = newword + e;}
+
+      	else{ newword = newword + word.charAt(i);}
+
+        }	
+
+
+
+     return newword;
     }
 }
--- UniqueChars.java
+++ UniqueChars.java
@@ -1,17 +1,42 @@
 /** String processing exercise 2. */
-public class UniqueChars {
-    public static void main(String[] args) {  
-        String str = args[0];
-        System.out.println(uniqueChars(str));
-    }
+public class UniqueChars { 
+    public static void main(String[] args) {
+     	String word = args[0];
+        System.out.println(UniqueChars(word)); }    
 
-    /**
-     * Returns a string which is identical to the original string, 
-     * except that all the duplicate characters are removed,
-     * unless they are space characters.
-     */
-    public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
-    }
+
+
+        //// what the method does:
+        /// checking if the char at the first index is uqule to the second
+        /// if it does it'll not be adding them to the new word
+        /// if its unique it'll add the char to the new word 
+        /// after im done it'll move to check if the char at the second index is uqule to the first
+
+        
+    public static String UniqueChars(String word) {
+        char first = word.charAt(0);
+         String newword = first + "";
+        int i = 1;
+        int len = word.length(); 
+
+        while(i<len){
+            char start = word.charAt(i);
+            int j = 0;
+
+            while (j<len){
+            char comp = word.charAt(j); /// finding the char in the j index to compare   
+                    if (comp == start){
+                        break;}  
+                    j++;}
+
+                    if (i==j){
+                    newword = newword + start;}               
+                    i++;} /// adding the unique char to the new word
+                              return newword;
+  
+            }
+        
+        
 }
+
+
